4956f935328e0520fadda9c75e7b8dfeeb4e3bcc
Alex Klyuev <aklyuev96@gmail.com>
2021-06-29 16:25:24 -0700

    create first input box with proper typescript

--- src/pages/create_account.tsx
+++ src/pages/create_account.tsx
@@ -1,27 +1,30 @@
 import Head from 'next/head';
-import { FormEvent, useState, useEffect } from 'react';
+import { ChangeEventHandler, FormEvent, useState } from 'react';
 import styles from 'src/styles/create_account.module.scss';
 
 export default function CreateAccount() {
-  const [count, setCount] = useState(0);
 
-  function handleSubmit(evt: FormEvent) {
-    evt.preventDefault();
+  // state
+  const [text, setText] = useState('');
 
-    // experimenting with hooks
-    setCount(count + 1);
-
-
-
-    /* const response = await fetch('/api/create_new_account', {
-      method: 'POST',
-      body: JSON.stringify({}),
-    }); */
-
-    console.log(/*await response.json(),*/ count);
+  // handlers
+  function handleChange(e: FormEvent) {
+    // designate target as an input element which is guaranteed to have a value property
+    const target = e.target as HTMLInputElement;
+    console.log(target.value);
+    setText(target.value);
   }
 
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
 
+    const response = await fetch('/api/create_new_account', {
+      method: 'POST',
+      body: JSON.stringify({}),
+    });
+
+    console.log(await response.json());
+  }
 
   return (
     <>
@@ -30,8 +33,8 @@ export default function CreateAccount() {
       </Head>
       <article className={styles.article}>
         <form className={styles.form} onSubmit={handleSubmit}>
+          <input name="username" value={text} onChange={handleChange} />
           <button>Create Account</button>
-          <p>You clicked me {count} times</p>
         </form>
       </article>
     </>
