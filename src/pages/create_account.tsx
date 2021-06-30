6896ead73ff72aab4a7c46250bf5d7d3fef0dfed
Alex Klyuev <aklyuev96@gmail.com>
2021-06-29 17:07:23 -0700

    separate out username as component

--- src/pages/create_account.tsx
+++ src/pages/create_account.tsx
@@ -1,19 +1,27 @@
 import Head from 'next/head';
-import { ChangeEventHandler, FormEvent, useState } from 'react';
+import { useState, ChangeEventHandler, FormEvent } from 'react';
 import styles from 'src/styles/create_account.module.scss';
+import Username from './components/Username';
 
 export default function CreateAccount() {
 
   // state
-  const [text, setText] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
 
   // handlers
-  function handleChange(e: FormEvent) {
+  function handleUsernameChange(username: string) {
+    setUsername(username);
+  }
+
+  console.log(username);
+  /* function handleChange(e: FormEvent) {
     // designate target as an input element which is guaranteed to have a value property
     const target = e.target as HTMLInputElement;
     console.log(target.value);
     setText(target.value);
-  }
+  } */
 
   async function handleSubmit(e: FormEvent) {
     e.preventDefault();
@@ -33,7 +41,8 @@ export default function CreateAccount() {
       </Head>
       <article className={styles.article}>
         <form className={styles.form} onSubmit={handleSubmit}>
-          <input name="username" value={text} onChange={handleChange} />
+          {/*<input name="username" value={text} onChange={handleChange} />*/}
+          <Username handleUsernameChange={handleUsernameChange} />
           <button>Create Account</button>
         </form>
       </article>
